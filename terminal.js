09ad1e6649f2ca154596b07ce674789398243f62
Ramon <Ramon@CompTech-Walther.de>
2020-10-16 19:04:13 +0200

    Cleaned up code

--- terminal.js
+++ terminal.js
@@ -2,108 +2,106 @@ class Terminal {
 	
 	//General settings
 	static header = "BetaLabsOS [Version 2.0.1] (c) 2020 MrRawbit";
-	static prefix = "root@BetaLabs.io:~#";
-	static startupPath = "./startup.txt";
+	static prefix = "root@BetaLabs.io:~# ";
 	
-    static init() {
-        Terminal.contentNode = document.getElementById("terminalContent");
-        Terminal.cursorNode = document.getElementById("terminalCursor");
-        Terminal.inputNode = document.getElementById("terminalInput");
-        Terminal.bufferNode = document.getElementById("terminalBuffer");
-        Terminal.history = new Array("");
-        Terminal.historyPos = 0;
-        Terminal.locks = 0;
-        Terminal.inputNode.addEventListener("focus", function() {
-            Terminal.cursorEvent = setInterval(function() { 
-                if(Terminal.cursorNode.hidden) {
-                    Terminal.cursorNode.hidden = false;
-                } else {
-                    Terminal.cursorNode.hidden = true;
-                }
+    static async init() {
+        this.contentNode = document.getElementById("terminalContent");
+        this.cursorNode = document.getElementById("terminalCursor");
+        this.inputNode = document.getElementById("terminalInput");
+        this.bufferNode = document.getElementById("terminalBuffer");
+        this.history = new Array("");
+        this.historyPos = 0;
+        this.locks = 0;
+        this.registerEvents();
+        this.inputNode.focus();
+        await this.startup();
+    }
+
+    static registerEvents() {
+        this.inputNode.addEventListener("focus", function() {
+            this.cursorEvent = setInterval(function() {
+                this.cursorNode.hidden = !this.cursorNode.hidden;
             }, 500);
         });
-        Terminal.inputNode.addEventListener("input", function() {
+        this.inputNode.addEventListener("input", function() {
             Terminal.updateBuffer();
         });
-        Terminal.inputNode.addEventListener("keyup", function(event) {
+        this.inputNode.addEventListener("keyup", function(event) {
             if (event.keyCode === 13) { // Return
-                let cmd = Terminal.inputNode.value;
+                let cmd = this.inputNode.value;
                 Terminal.println(cmd);
-                if(Terminal.history[Terminal.history.length-1] !== cmd) {
-                    Terminal.history.push(cmd);
+                if(this.history[this.history.length-1] !== cmd) {
+                    this.history.push(cmd);
                 }
-                Terminal.historyPos = Terminal.history.length;
-                Terminal.inputNode.value = "";
-                Terminal.bufferNode.innerHTML = "";
+                this.historyPos = this.history.length;
+                this.inputNode.value = "";
+                this.bufferNode.innerHTML = "";
                 Terminal.execute(cmd);
-            } else if (Terminal.history.length > 1 && (event.keyCode === 38 || event.keyCode === 40)) {
-                if (event.keyCode === 38 && Terminal.historyPos > 1) { // Arrow key up
-                    Terminal.inputNode.value = Terminal.history[--Terminal.historyPos];
-                } else if (event.keyCode === 40 && Terminal.historyPos < Terminal.history.length) { // Arrow key down
-                    if(Terminal.historyPos === Terminal.history.length-1) {
-                        Terminal.inputNode.value = Terminal.history[0];
-                        Terminal.historyPos = Terminal.history.length;
+            } else if (this.history.length > 1 && (event.keyCode === 38 || event.keyCode === 40)) {
+                if (event.keyCode === 38 && this.historyPos > 1) { // Arrow key up
+                    this.inputNode.value = this.history[--this.historyPos];
+                } else if (event.keyCode === 40 && this.historyPos < this.history.length) { // Arrow key down
+                    if(this.historyPos === this.history.length-1) {
+                        this.inputNode.value = this.history[0];
+                        this.historyPos = this.history.length;
                     } else {
-                        Terminal.inputNode.value = Terminal.history[++Terminal.historyPos];
+                        this.inputNode.value = this.history[++this.historyPos];
                     }
                 }
                 Terminal.updateBuffer();
             }
         });
-        Terminal.inputNode.addEventListener("blur", function() {
-            clearInterval(Terminal.cursorEvent);
-            Terminal.cursorNode.hidden = false;
+        this.inputNode.addEventListener("blur", function() {
+            clearInterval(this.cursorEvent);
+            this.cursorNode.hidden = false;
         });
         document.getElementById("terminalWindow").addEventListener("click", function() {
-            Terminal.inputNode.focus();
+            this.inputNode.focus();
         });
-        Terminal.inputNode.focus();
-		//Boot
-		Terminal.startup();
     }
 
     static rawPrint(text) {
         let node = document.createElement("span");
-        let textNode = document.createTextNode(text);
+        let textNode = document.createTextNode(text.replace(/ /g, "\u00A0"));
         node.appendChild(textNode);
-        Terminal.contentNode.insertBefore(node, Terminal.bufferNode);
+        this.contentNode.insertBefore(node, this.bufferNode);
     };
 
     static rawReprint(text) {
-        let node = Terminal.bufferNode.previousSibling;
+        let node = this.bufferNode.previousSibling;
         while(node.tagName.toLowerCase() !== 'span') {
             node = node.previousSibling;
         }
-        node.textContent = text;
+        node.textContent = text.replace(/ /g, "\u00A0");
     };
 
     static updateBuffer() {
-        Terminal.bufferNode.innerHTML = "";
-        Terminal.bufferNode.appendChild(document.createTextNode(Terminal.inputNode.value));
-        Terminal.inputNode.focus();
+        this.bufferNode.innerHTML = "";
+        this.bufferNode.appendChild(document.createTextNode(this.inputNode.value));
+        this.inputNode.focus();
     }
 
     static newLine() {
         let node = document.createElement("br");
-        Terminal.contentNode.insertBefore(node, Terminal.bufferNode);
+        this.contentNode.insertBefore(node, this.bufferNode);
     };
 
     static updateScrollPos() {
-        Terminal.contentNode.scrollTop = Terminal.contentNode.scrollHeight;
+        this.contentNode.scrollTop = this.contentNode.scrollHeight;
     };
 
     static lockInput() {
-        ++Terminal.locks;
-        if(Terminal.locks) {
-            Terminal.inputNode.disabled = true;
+        ++this.locks;
+        if(this.locks) {
+            this.inputNode.disabled = true;
         }
     };
 
     static unlockInput() {
-        --Terminal.locks;
-        if(!Terminal.locks) {
-            Terminal.inputNode.disabled = false;
-            Terminal.inputNode.focus();
+        --this.locks;
+        if(!this.locks) {
+            this.inputNode.disabled = false;
+            this.inputNode.focus();
         }
     };
 
@@ -115,17 +113,17 @@ class Terminal {
         let textBuffer = "";
 		for (let c of text) {
             if (c === '\n') {
-                Terminal.rawPrint(textBuffer);
+                this.rawPrint(textBuffer);
                 textBuffer = "";
-                Terminal.newLine();
+                this.newLine();
             } else {
                 textBuffer += c;	
             }
         }
         if (textBuffer !== "") {
-            Terminal.rawPrint(textBuffer);
+            this.rawPrint(textBuffer);
         }
-        Terminal.updateScrollPos();
+        this.updateScrollPos();
     };
 
     static reprint(text) {
@@ -133,52 +131,57 @@ class Terminal {
         for (let i = 0; i < text.length; ++i) {
             let c = text.charAt(i);
             if (c === '\n') {
-                Terminal.rawReprint(textBuffer);
-                Terminal.newLine();
+                this.rawReprint(textBuffer);
+                this.newLine();
                 let remainingText = text.substring(i+1);
                 if (remainingText !== "") {
-                    Terminal.print(textBuffer);
+                    this.print(textBuffer);
                 }
             } else {
                 textBuffer += c;
             }
         }
         if (textBuffer !== "") {
-            Terminal.rawReprint(textBuffer);
+            this.rawReprint(textBuffer);
         }
-        Terminal.updateScrollPos();
+        this.updateScrollPos();
     };
 	
 	static async write(text) {
-        Terminal.lockInput();
+        this.lockInput();
         for (let i = 0; i < text.length; ++i) {
             let out = text.substring(0, i+1);
             if(!i) {
-                Terminal.print(out);
+                this.print(out);
             } else {
-                Terminal.reprint(out);
+                this.reprint(out);
             }
-			await Terminal.sleep(25);
+			await this.sleep(25);
 		}
-        Terminal.unlockInput();
+        this.unlockInput();
 	}
 
     static println(text) {
-        Terminal.print(text + '\n');
+        this.print(text + '\n');
     };
 	
 	static async writeln(text) {
-        await Terminal.write(text + '\n');
+        await this.write(text + '\n');
     };
+
+	static clear() {
+
+    }
 
     static execute(input) {
         console.log("Terminal.execute(\"" + input + "\");");
+        this.print(this.prefix);
     }
 	
 	//Output the startup message
 	static async startup() {
-        Terminal.lockInput();
-        Terminal.print("Initializing... [0%");
+        this.lockInput();
+        this.print("Initializing... [0%");
         for (let i = 0; i <= 100; ++i) {
             let progress = "";
             for (let j = 0; j < 20; ++j) {
@@ -188,43 +191,37 @@ class Terminal {
                     if(progress.endsWith("=")) {
                         progress += ">";
                     }
-                    progress += "\u00A0";
+                    progress += " ";
                 }
             }
-            Terminal.reprint("Initializing... [" + progress + "] (" + i + "%)");
-            await Terminal.sleep(15);
+            this.reprint("Initializing... [" + progress + "] (" + i + "%)");
+            await this.sleep(10);
         }
-        Terminal.reprint("Starting.");
-        await Terminal.sleep(500);
-        Terminal.reprint("Starting..");
-        await Terminal.sleep(500);
-        Terminal.reprint("Starting...");
-        await Terminal.sleep(500);
-        Terminal.reprint(" ");
-        await Terminal.write(Terminal.header);
-        await Terminal.sleep(1000);
-        Terminal.newLine();
-		/*
-		let fileData = Terminal.loadFile(Terminal.startupPath); //Get content of the startup file
-		var lines = fileData.split('\n'); //Split it in its lines
-		for (var line=0; line<lines.length; line++) { //Print every line by line with a delay between them
-			Terminal.println(lines[line]);
-			await Terminal.sleep(200);
-		}
-		await Terminal.sleep(1000);
-		*/
-        Terminal.unlockInput();
-	}
-	//Read a linked file
-	static loadFile(filePath) {
-		var result = null;
-		var xmlhttp = new XMLHttpRequest();
-		xmlhttp.open("GET", filePath, false);
-		xmlhttp.send();
-		if (xmlhttp.status === 200) {
-			result = xmlhttp.responseText;
-		}
-		return result;
+        this.reprint("Starting.");
+        await this.sleep(500);
+        this.reprint("Starting..");
+        await this.sleep(500);
+        this.reprint("Starting...");
+        await this.sleep(500);
+        this.reprint(" ");
+        await this.write(this.header);
+        await this.sleep(1000);
+        this.newLine();
+        this.println("  ____       _        _           _           _       ");
+		await this.sleep(200);
+        this.println(" |  _ \\     | |      | |         | |         (_)      ");
+        await this.sleep(200);
+        this.println(" | |_) | ___| |_ __ _| |     __ _| |__  ___   _  ___  ");
+        await this.sleep(200);
+        this.println(" |  _ < / _ \\ __/ _` | |    / _` | '_ \\/ __| | |/ _ \\ ");
+        await this.sleep(200);
+        this.println(" | |_) |  __/ || (_| | |___| (_| | |_) \\__ \\_| | (_) |");
+        await this.sleep(200);
+        this.println(" |____/ \\___|\\__\\__,_|______\\__,_|_.__/|___(_)_|\\___/ ");
+		await this.sleep(1000);
+        this.newLine();
+        this.unlockInput();
+        this.print(this.prefix);
 	}
 
 }
